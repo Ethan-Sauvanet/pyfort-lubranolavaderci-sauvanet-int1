ebb45f573cff7bbbdfbe41e79a831210955bf280
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2024-12-17 16:00:41 +0100

    Chance module finished

--- Game/Challenges/chance_challenges.py
+++ Game/Challenges/chance_challenges.py
@@ -1,11 +1,15 @@
 import random
 
+def chance_challenges() :
+    challenges = [shell_game, roll_dice_game]
+    challenge = random.choice(challenges)
+    return challenge()
+
 def shell_game() :
 
-    global good_shell
     shells = ['A','B','C']
     attempts = 2
-    print("Welcome to Shell Game\n" +
+    print("Welcome to Shell Game !\n" +
           "In this game, you must guess which of the three shells (A, B, or C) hides the key.\n" +
           "You have two attempts to find it.\n")
     print("You can choose the shell (A, B, or C) by typing the letter corresponding.\n")
@@ -37,24 +41,50 @@ def shell_game() :
 
     print("You lost, the key was under the shell :", good_shell)
     return False
-shell_game()
+
 
 def roll_dice_game() :
 
-    print("Welcome to Roll Dice Game" +
-    "In this game, the game master and you have 3 attempts to roll two dices." +
+    print("Welcome to Roll Dice Game !\n" +
+    "In this game, the game master and you have 3 attempts to roll two dices.\n" +
     "The first one to do a 6 with one of his dices win the game !\n")
 
     attempts = 3
 
     for i in range(attempts) :
-        print("You have", attempts, "attempts left.")
+        print("You have", attempts - i, "attempts left.")
 
-        enter_key = ord(input("Press ENTER to roll the dice : "))
-        while enter_key != 13 :
-            enter_key = ord(input("Press ENTER to roll the dice : "))
-            if enter_key != 13 :
-                print("You have entered the wrong key.")
+        enter_key = input("Press ENTER to roll the dice : ")
+        while enter_key != "" :
+            print("You have entered the wrong key.")
+            enter_key = input("Press ENTER to roll the dice : ")
+
+        rolls = (random.randint(1,6), random.randint(1,6))
+        print("\nYour rolls : [",rolls[0],"] and [",rolls[1],"]")
+
+        for item in rolls :
+            if item == 6 :
+                print("You rolled 6 ! You win the game!\n" +
+                      "The key is yours, you can continue.")
+                return True
+        print("You didn't roll a 6. Game master's turn !")
+
+        master_rolls = (random.randint(1, 6), random.randint(1, 6))
+        print("\nMaster rolls : [", master_rolls[0], "] and [", master_rolls[1], "]")
+
+        for item in master_rolls :
+            if item == 6 :
+                print("The Master rolled a 6 ! You lost the game!\n")
+                return False
+        print("Neither player rolled a 6. The game continue !\n")
+
+    print("No player has scored a 6 after 3 tries. It's a draw !\n")
+    return False
+
+chance_challenges()
+
+
+
 
 
 
