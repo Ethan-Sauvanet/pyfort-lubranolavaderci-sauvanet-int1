b8c5bd45ffa2e8a8751fc85e603a2f14b1171374
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2024-12-17 14:54:23 +0100

    Shell game improved

--- Game/Challenges/chance_challenges.py
+++ Game/Challenges/chance_challenges.py
@@ -2,6 +2,7 @@ import random
 
 def shell_game() :
 
+    global good_shell
     shells = ['A','B','C']
     attempts = 2
     print("Welcome to Shell Game\n" +
@@ -13,7 +14,11 @@ def shell_game() :
         good_shell = random.choice(shells)
         print("You have", attempts, "attempts left")
 
-        guess = input("Guess the shell that contains the key: ")
+        print("The Master hides the key,")
+        print(" ---    ---    ---")
+        print("| A |  | B |  | C |")
+        print(" ---    ---    ---")
+        guess = input("Guess the shell that contains the key : ")
 
         guess_int = ord(guess)
         if 97 <= guess_int <= 122:
@@ -32,6 +37,25 @@ def shell_game() :
 
     print("You lost, the key was under the shell :", good_shell)
     return False
+shell_game()
+
+def roll_dice_game() :
+
+    print("Welcome to Roll Dice Game" +
+    "In this game, the game master and you have 3 attempts to roll two dices." +
+    "The first one to do a 6 with one of his dices win the game !\n")
+
+    attempts = 3
+
+    for i in range(attempts) :
+        print("You have", attempts, "attempts left.")
+
+        enter_key = ord(input("Press ENTER to roll the dice : "))
+        while enter_key != 13 :
+            enter_key = ord(input("Press ENTER to roll the dice : "))
+            if enter_key != 13 :
+                print("You have entered the wrong key.")
+
 
 
 
