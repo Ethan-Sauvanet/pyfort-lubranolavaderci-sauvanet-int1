54ffac461038a8d8ba8cf0e9a2f0a9806011eecd
Ethan-Sauvanet <ethan.sauvanet@efrei.net>
2025-01-02 18:23:16 +0100

    Working on logical challenges

--- Game/logical_challenges.py
+++ Game/logical_challenges.py
@@ -3,38 +3,58 @@ def next_player(player) :
     return 1 - player
 
 def empty_grid() :
-    empty_grid = [0][0]
+    grid = [[0]*3 for i in range(3)]
     for i in range(3):
         for j in range(3):
-            empty_grid[i][j] = " "
-    return empty_grid
+            grid[i][j] = " "
+    return grid
 
 def display_grid(grid,message) :
     print(message)
     for i in range(3):
         for j in range(3):
-            print("| ", grid[i][j], end=" " + "| ")
-        print("\n")
-    print("______________")
+            print("|", grid[i][j], end=" " + "| ")
+        print("")
+    print("_________________")
     return
 
 def ask_position():
     while True:
-        user_input = input("Enter a position in the format row,column: ")
+        user_input = input("Enter a position for a boat in the format row,column: ")
 
         parts = user_input.split(",")
         if len(parts) == 2:
             row_str, column_str = parts[0].strip(), parts[1].strip()
 
-            row = int(row_str)
-            column = int(column_str)
+            row = int(row_str) - 1
+            column = int(column_str) - 1
 
-            if 1 <= row < 4 and 1 <= column < 4:
+            if 0 <= row < 3 and 0 <= column < 3:
                 return (row, column)
             else:
-                print("Position out of bounds. Please enter values between 0 and 3.")
+                print("Position out of bounds. Please enter values between 1 and 3.")
         else:
             print("Invalid format. Please enter the position as row,column (e.g.: 1,2).")
+
+def initialise() :
+    grid = empty_grid()
+    display_grid(grid,"\nThis is your grid, fill it up by placing 2 boats (B) :")
+
+    position1 = ask_position()
+    position2 = position1
+    while position1 == position2:
+        position2 = ask_position()
+
+    a, b, c, d = position1[0], position1[1], position2[0], position2[1]
+
+    grid[a][b] = 'B'
+    grid[c][d] = 'B'
+
+    display_grid(grid, "\nThis is your grid filled with your boats :")
+    return
+
+def turn(player, player_shots_grid, opponent_grid) :
+
 
 
 
